e1b2c1ec1095b0d0221121b6ec4ad56754cab254
JaeHyeokee <jaehyeok817@naver.com>
2024-12-10 02:17:45 +0900

    20241210 SKILLS 완성

--- src/jsx/Skills.jsx
+++ src/jsx/Skills.jsx
@@ -1,11 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Style from '../css/Skills.module.css';
+import Data from '../data/Skills.json';
+
+const groupByType = (data) => {
+    return data.reduce((acc, item) => {
+        if (!acc[item.type]) acc[item.type] = [];
+        acc[item.type].push(item);
+        return acc;
+    }, {});
+};
 
 const Skills = () => {
+    const [groupedData, setGroupedData] = useState({});
+
+    useEffect(() => {
+        const grouped = groupByType(Data);
+        setGroupedData(grouped);
+    }, []);
+
     return (
-        <div id="skills" className={Style.section}>
+        <section id="skills" className={Style.section}>
             <span className={Style.title}>SKILLS</span>
-        </div>
+            <div className={Style.skillFrame}>
+            {Object.keys(groupedData).map((type) => (
+                <div key={type} className={Style.group}>
+                    <span className={Style.typeLabel}>{type}</span>
+                    <div className={Style.type}>
+                        {groupedData[type].map((item) => (
+                            <div key={item.id} className={Style.item}>
+                                <img className={Style.icon} src={item.icon} alt={item.name}/>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            ))}
+            </div>
+        </section>
     );
 };
 
