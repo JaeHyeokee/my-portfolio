c37dd723857885b617ae38b964c99002c8704d3f
JaeHyeokee <jaehyeok817@naver.com>
2024-12-13 16:02:46 +0900

    20241213 호스트 naver->gmail 변경

--- server.js
+++ server.js
@@ -14,11 +14,11 @@ const transporter = nodemailer.createTransport({
     service: process.env.EMAIL_SERV,
     host: process.env.EMAIL_HOST,
     port: PORT,
+    secure: false,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
-    },
-    secure: false,
+    }
 });
 
 app.post('/send-email', (req, res) => {
