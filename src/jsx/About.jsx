6dfef97b83b170cc6adc08ebc7b7138d867b355d
JaeHyeokee <jaehyeok817@naver.com>
2024-12-15 22:09:30 +0900

    ABOUT 반응형 추가

--- src/jsx/About.jsx
+++ src/jsx/About.jsx
@@ -13,37 +13,61 @@ const About = () => {
         <section id="about" className={Style.section}>
             <span className={Style.title}>ABOUT</span>
             <div className={Style.profileSection}>
-                <img src={ProfileImg} alt='profileImg'/>
-                <div className={Style.profileFrame}>
-                    <ul className={Style.profile}>
-                        <li className={Style.profileInfo}>
-                            <img className={Style.icon} src={Name} alt='name'/>
-                            <p className={Style.info}><label className={Style.label}>이름<br/></label>이재혁</p>
-                        </li>
-                        <li className={Style.profileInfo}>
-                            <img className={Style.icon} src={Birth} alt='birth'/>    
-                            <p className={Style.info}><label className={Style.label}>생년월일<br/></label>1997.08.17</p>
-                        </li>
-                        <li className={Style.profileInfo}>
-                            <img className={Style.icon} src={Residence} alt='residence'/>    
-                            <p className={Style.info}><label className={Style.label}>거주 지역<br/></label>경기도 성남시 분당구</p>
-                        </li>
-                    </ul>
-                    <ul className={Style.profile}>
-                        <li className={Style.profileInfo}>
-                            <img className={Style.icon} src={Education} alt='education'/>    
-                            <p className={Style.info}><label className={Style.label}>최종 학력<br/></label>세종대학교 소프트웨어학과</p>
-                        </li>
-                        <li className={Style.profileInfo}>
-                            <img className={Style.icon} src={Phone} alt='phone'/>    
-                            <p className={Style.info}><label className={Style.label}>연락처<br/></label>010-4678-3712</p>
-                        </li>
-                        <li className={Style.profileInfo}>
-                            <img className={Style.icon} src={Email} alt='email'/>    
-                            <p className={Style.info}><label className={Style.label}>이메일<br/></label>jaehyeok817@naver.com</p>
-                        </li>
-                    </ul>
-                </div>
+                <img className={Style.profileImg} src={ProfileImg} alt='profileImg'/>
+                <ul className={Style.profile}>
+                    <li className={Style.profileInfo}>
+                        <img className={Style.icon} src={Name} alt='name'/>
+                        <p className={Style.info}><label className={Style.label}>이름<br/></label>이재혁</p>
+                    </li>
+                    <li className={Style.profileInfo}>
+                        <img className={Style.icon} src={Birth} alt='birth'/>    
+                        <p className={Style.info}><label className={Style.label}>생년월일<br/></label>1997.08.17</p>
+                    </li>
+                    <li className={Style.profileInfo}>
+                        <img className={Style.icon} src={Residence} alt='residence'/>    
+                        <p className={Style.info}><label className={Style.label}>거주 지역<br/></label>경기도 성남시 분당구</p>
+                    </li>
+                </ul>
+                <ul className={Style.profile}>
+                    <li className={Style.profileInfo}>
+                        <img className={Style.icon} src={Education} alt='education'/>    
+                        <p className={Style.info}><label className={Style.label}>최종 학력<br/></label>세종대학교 소프트웨어학과</p>
+                    </li>
+                    <li className={Style.profileInfo}>
+                        <img className={Style.icon} src={Phone} alt='phone'/>    
+                        <p className={Style.info}><label className={Style.label}>연락처<br/></label>010-4678-3712</p>
+                    </li>
+                    <li className={Style.profileInfo}>
+                        <img className={Style.icon} src={Email} alt='email'/>    
+                        <p className={Style.info}><label className={Style.label}>이메일<br/></label>jaehyeok817@naver.com</p>
+                    </li>
+                </ul>
+                <ul className={Style.profileMobile}>
+                    <li className={Style.profileInfo}>
+                        <img className={Style.icon} src={Name} alt='name'/>
+                        <p className={Style.info}><label className={Style.label}>이름<br/></label>이재혁</p>
+                    </li>
+                    <li className={Style.profileInfo}>
+                        <img className={Style.icon} src={Education} alt='education'/>    
+                        <p className={Style.info}><label className={Style.label}>최종 학력<br/></label>세종대학교 소프트웨어학과</p>
+                    </li>
+                    <li className={Style.profileInfo}>
+                        <img className={Style.icon} src={Birth} alt='birth'/>    
+                        <p className={Style.info}><label className={Style.label}>생년월일<br/></label>1997.08.17</p>
+                    </li>
+                    <li className={Style.profileInfo}>
+                        <img className={Style.icon} src={Residence} alt='residence'/>    
+                        <p className={Style.info}><label className={Style.label}>거주 지역<br/></label>경기도 성남시 분당구</p>
+                    </li>
+                    <li className={Style.profileInfo}>
+                        <img className={Style.icon} src={Phone} alt='phone'/>    
+                        <p className={Style.info}><label className={Style.label}>연락처<br/></label>010-4678-3712</p>
+                    </li>
+                    <li className={Style.profileInfo}>
+                        <img className={Style.icon} src={Email} alt='email'/>    
+                        <p className={Style.info}><label className={Style.label}>이메일<br/></label>jaehyeok817@naver.com</p>
+                    </li>
+                </ul>
             </div>
         </section>
     );
